7042e4d0104f48a9109814d743e6ef2c1f6f2a61
dpdms529 <musiclove529@naver.com>
2025-10-10 12:17:41 +0900

    03_pip.py

---
+++ section1010/03_pip.py
@@ -0,0 +1,15 @@
+# pip 명령어 : 파이썬 확장 모듈을 설치, 삭제할 수 잇는 기능 제공
+"""
+>python --version                        파이선 버전 확인
+>python -m pip install --upgrade pip     pip명령 업그레이드
+
+>pip list                                파이선 모듈 및 패키지 목록 확인
+>pip freeze
+>pip install 모듈명                       파이선 모듈 설치
+>pip install 모듈명==버전                 원하는 버전으로 모듈 설치
+>pip uninstall -y 모듈명                  파이선 모듈 삭제
+
+>pip install numpy              numpy 모듈 설치
+>pip install wordcloud          wordcloud 모듈 설치
+>pip uninstall -y wordcloud     wordcloud 모듈 삭제
+"""
